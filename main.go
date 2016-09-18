c693d461a1c0094d2948ebe35b0e55ad88febf73
Seth Ammons <seth@sendgrid.com>
2016-09-18 07:48:56 -0700

    tests pass but output looks the same. wtf

---
+++ main.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"bufio"
+	"encoding/json"
+	"fmt"
+	"io"
+	"os"
+)
+
+func main() {
+	scanner := bufio.NewScanner(os.Stdin)
+	formatter(scanner, os.Stdout)
+}
+
+func formatter(s *bufio.Scanner, w io.Writer) {
+	for s.Scan() {
+		line := s.Text()
+		jsonStart := 0
+		for i, thisRune := range line {
+			if string(thisRune) == "{" {
+				jsonStart = i
+				break
+			}
+		}
+		date := line[:jsonStart]
+		rawJSON := line[jsonStart:]
+		// todo: handle arbitrary nesting
+		var err error
+		var buf []byte
+		data := make(map[string]interface{})
+		// todo: don't use strings, just bytes
+		if err = json.Unmarshal([]byte(rawJSON), &data); err != nil {
+			w.Write([]byte(line))
+			w.Write([]byte("\n"))
+			continue
+		}
+		if buf, err = json.MarshalIndent(data, " ", "    "); err != nil {
+			w.Write([]byte(fmt.Sprintf("\n >> [ERROR] %v \n", err)))
+		}
+		w.Write([]byte(date))
+		var escaping bool
+		for _, thisRune := range buf {
+			if string(thisRune) == `\` && !escaping {
+				escaping = true
+				continue
+			} else if string(thisRune) == "n" && escaping {
+				w.Write([]byte("\n"))
+				escaping = false
+				continue
+			} else if string(thisRune) == "t" && escaping {
+				w.Write([]byte("    "))
+				escaping = false
+				continue
+			} else {
+				escaping = false
+			}
+			w.Write([]byte{thisRune})
+		}
+	}
+}
---
+++ main_test.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"bufio"
+	"bytes"
+	"io/ioutil"
+	"strings"
+	"testing"
+)
+
+func TestFormatter(t *testing.T) {
+	input := `2016/09/18 06:26:10 starting on :9126
+2016/09/18 06:26:13 {"event":"panic","message":"interface conversion: interface {} is nil, not *main.logWriter\ngoroutine 5 [running]:\nruntime/debug.Stack(0xc42003f8e0, 0x25bb60, 0xc420014840)\n\t/usr/local/go/src/runtime/debug/stack.go:24 +0x79\nmain.mwPanic.func1.1(0xc4200d01e0)\n\t/Users/sethammons/workspace/go/src/github.com/sethgrid/cloudlock/main.go:73 +0x76\npanic(0x25bb60, 0xc420014840)\n\t/usr/local/go/src/runtime/panic.go:458 +0x243\nmain.mwLog.func1(0x3b8d40, 0xc420075930, 0xc4200d01e0)\n\t/Users/sethammons/workspace/go/src/github.com/sethgrid/cloudlock/main.go:126 +0x8fc\nnet/http.HandlerFunc.ServeHTTP(0xc4200cca60, 0x3b8d40, 0xc420075930, 0xc4200d01e0)\n\t/usr/local/go/src/net/http/server.go:1726 +0x44\nmain.mwPanic.func1(0x3b8d40, 0xc420075930, 0xc4200d01e0)\n\t/Users/sethammons/workspace/go/src/github.com/sethgrid/cloudlock/main.go:76 +0x8b\nnet/http.HandlerFunc.ServeHTTP(0xc4200cca80, 0x3b8d40, 0xc420075930, 0xc4200d01e0)\n\t/usr/local/go/src/net/http/server.go:1726 +0x44\nnet/http.(*ServeMux).ServeHTTP(0x3d3720, 0x3b8d40, 0xc420075930, 0xc4200d01e0)\n\t/usr/local/go/src/net/http/server.go:2022 +0x7f\nnet/http.serverHandler.ServeHTTP(0xc420092500, 0x3b8d40, 0xc420075930, 0xc4200d01e0)\n\t/usr/local/go/src/net/http/server.go:2202 +0x7d\nnet/http.(*conn).serve(0xc420092580, 0x3b92c0, 0xc420014740)\n\t/usr/local/go/src/net/http/server.go:1579 +0x4b7\ncreated by net/http.(*Server).Serve\n\t/usr/local/go/src/net/http/server.go:2293 +0x44d\n"}`
+
+	want := `2016/09/18 06:26:10 starting on :9126
+2016/09/18 06:26:13 {
+     "event": "panic",
+     "message": "interface conversion: interface {} is nil, not *main.logWriter
+goroutine 5 [running]:
+runtime/debug.Stack(0xc42003f8e0, 0x25bb60, 0xc420014840)
+    /usr/local/go/src/runtime/debug/stack.go:24 +0x79
+main.mwPanic.func1.1(0xc4200d01e0)
+    /Users/sethammons/workspace/go/src/github.com/sethgrid/cloudlock/main.go:73 +0x76
+panic(0x25bb60, 0xc420014840)
+    /usr/local/go/src/runtime/panic.go:458 +0x243
+main.mwLog.func1(0x3b8d40, 0xc420075930, 0xc4200d01e0)
+    /Users/sethammons/workspace/go/src/github.com/sethgrid/cloudlock/main.go:126 +0x8fc
+net/http.HandlerFunc.ServeHTTP(0xc4200cca60, 0x3b8d40, 0xc420075930, 0xc4200d01e0)
+    /usr/local/go/src/net/http/server.go:1726 +0x44
+main.mwPanic.func1(0x3b8d40, 0xc420075930, 0xc4200d01e0)
+    /Users/sethammons/workspace/go/src/github.com/sethgrid/cloudlock/main.go:76 +0x8b
+net/http.HandlerFunc.ServeHTTP(0xc4200cca80, 0x3b8d40, 0xc420075930, 0xc4200d01e0)
+    /usr/local/go/src/net/http/server.go:1726 +0x44
+net/http.(*ServeMux).ServeHTTP(0x3d3720, 0x3b8d40, 0xc420075930, 0xc4200d01e0)
+    /usr/local/go/src/net/http/server.go:2022 +0x7f
+net/http.serverHandler.ServeHTTP(0xc420092500, 0x3b8d40, 0xc420075930, 0xc4200d01e0)
+    /usr/local/go/src/net/http/server.go:2202 +0x7d
+net/http.(*conn).serve(0xc420092580, 0x3b92c0, 0xc420014740)
+    /usr/local/go/src/net/http/server.go:1579 +0x4b7
+created by net/http.(*Server).Serve
+    /usr/local/go/src/net/http/server.go:2293 +0x44d
+"
+ }`
+	var b []byte
+	buf := bytes.NewBuffer(b)
+	// change input to a new scanner
+	formatter(bufio.NewScanner(strings.NewReader(input)), buf)
+
+	output, err := ioutil.ReadAll(buf)
+	if err != nil {
+		t.Errorf("error reading output - %v", err)
+	}
+	if string(output) != want {
+		t.Errorf("got: %v\n\nwant: %v", string(output), want)
+	}
+}
